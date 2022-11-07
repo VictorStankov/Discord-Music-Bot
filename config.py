6db5013ffcad966c200c215b805a4cc9493ed2f5
VictorStankov <viktorstankov1@gmail.com>
2022-11-08 00:50:04 +0200

    feat: Add config entry for OS detection
    Signed-off-by: VictorStankov <viktorstankov1@gmail.com>

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 from configparser import ConfigParser
+import os
 
 cfg = ConfigParser()
-cfg.read("config.cfg")
+cfg.read('config.cfg')
+
+token = cfg['Information']['Token']
+operating_system = cfg['Information']['Operating_System'].lower()
 
 token = cfg["Credentials"]["Token"]
